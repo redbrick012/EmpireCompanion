e0dc842809fffd3f3ac3a3050bbfca2b8f0efd7b
redbrick012 <157280932+redbrick012@users.noreply.github.com>
2026-08-06 20:30:25 +0100

    Update storage.js

--- V2/storage.js
+++ V2/storage.js
@@ -1,1 +1,83 @@
+/*
+==========================================
+Empire Companion V2
+Storage Manager
+==========================================
+*/
 
+const Storage = {
+
+    KEY: "empire-companion-v2",
+
+    saveCharacter(character) {
+
+        try {
+
+            localStorage.setItem(
+                this.KEY,
+                JSON.stringify(character)
+            );
+
+            console.log(
+                "Character saved."
+            );
+
+            return true;
+
+        } catch (error) {
+
+            console.error(
+                "Could not save character.",
+                error
+            );
+
+            return false;
+
+        }
+
+    },
+
+    getCharacter() {
+
+        try {
+
+            const data =
+                localStorage.getItem(
+                    this.KEY
+                );
+
+            if (!data)
+                return null;
+
+            return JSON.parse(data);
+
+        } catch (error) {
+
+            console.error(
+                "Could not load character.",
+                error
+            );
+
+            return null;
+
+        }
+
+    },
+
+    deleteCharacter() {
+
+        localStorage.removeItem(
+            this.KEY
+        );
+
+    },
+
+    hasCharacter() {
+
+        return (
+            this.getCharacter() !== null
+        );
+
+    }
+
+};
