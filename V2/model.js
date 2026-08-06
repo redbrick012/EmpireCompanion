cea4720d34a742c5e6b49b79f5d876eb39eff9c3
redbrick012 <157280932+redbrick012@users.noreply.github.com>
2026-08-06 21:20:58 +0100

    Create model.js

---
+++ V2/model.js
@@ -0,0 +1,69 @@
+/*
+==========================================
+Empire Companion
+Character Model
+Version 1
+==========================================
+*/
+
+const CharacterModel = {
+
+    create() {
+
+        return {
+
+            version: 1,
+
+            imported: null,
+
+            source: "Profound Decisions",
+
+            details: {
+
+                name: "",
+
+                cid: "",
+
+                nation: "",
+
+                lineage: "",
+
+                archetype: "",
+
+                virtue: "",
+
+                banner: "",
+
+                coven: "",
+
+                sect: "",
+
+                territory: "",
+
+                resource: "",
+
+                level: "",
+
+                status: "",
+
+                pointsSpent: ""
+
+            },
+
+            skills: [],
+
+            spells: [],
+
+            rituals: [],
+
+            ribbons: [],
+
+            bondedItems: [],
+
+            background: ""
+
+        };
+
+    }
+
+};
