209fba9cd4994f1ef93e248e93038d18120723a0
redbrick012 <157280932+redbrick012@users.noreply.github.com>
2026-08-06 21:02:50 +0100

    Update importer.js

--- V2/importer.js
+++ V2/importer.js
@@ -1,7 +1,39 @@
-const text = await file.text();
+const Importer = {
 
-const character = Parser.parse(text);
+    async importFile(file) {
 
-Storage.saveCharacter(character);
+        alert("1. File selected: " + file.name);
 
-App.loadCharacter(character);
+        try {
+
+            const text = await file.text();
+
+            alert("2. File read (" + text.length + " characters)");
+
+            const data = Parser.parse(text);
+
+            alert("3. Parser finished");
+
+            console.log(data);
+
+            App.characterImported(data);
+
+            alert("4. Character imported");
+
+        }
+        catch (e) {
+
+            console.error(e);
+
+            alert(
+                "ERROR:\n\n" +
+                e.message +
+                "\n\n" +
+                e.stack
+            );
+
+        }
+
+    }
+
+};
