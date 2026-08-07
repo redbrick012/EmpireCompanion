9e6cd925eca56b7446a85d4067c3782a68abf581
redbrick012 <157280932+redbrick012@users.noreply.github.com>
2026-08-07 09:11:48 +0100

    Update importer.js

--- V2/importer.js
+++ V2/importer.js
@@ -1,10 +1,3 @@
-/*
-==========================================
-Empire Companion
-Importer
-==========================================
-*/
-
 const Importer = {
 
     async importFile(file) {
@@ -14,23 +7,15 @@ const Importer = {
             const text = await file.text();
 
             const character = Parser.parse(text);
-            console.log(character);
-alert(JSON.stringify(character.details, null, 2));
 
-            // Save character locally
-            Storage.saveCharacter(character);
-
-            // Tell the app we've imported a character
             App.characterImported(character);
-
-            console.log(character);
 
         } catch (error) {
 
             console.error(error);
 
             alert(
-                "Import failed.\n\n" +
+                "Import failed\n\n" +
                 error.message
             );
 
